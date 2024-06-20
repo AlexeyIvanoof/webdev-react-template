251546f00ca0fca4dd885850d57faddf0be32e6f
Alexei Ivanov <alekseyivanoof34@gmail.com>
2024-06-20 21:21:30 +0700

    Hw-2 на проверку

--- music/src/components/filterTrack/filterTrack.tsx
+++ music/src/components/filterTrack/filterTrack.tsx
@@ -1,19 +1,30 @@
+"use client";
+
 import styles from "./filterTrack.module.css";
-import classNames from "classnames";
+import TracksFilterCategory from "./filterTrackMenu";
+import { useState } from "react";
+import { filterData } from "./data";
 
 export default function Filter() {
+
+  const [filterValue, setFilterValue] = useState<string | null>(null);
+  const handleFilterValue = (value: string) => {
+    setFilterValue((prev) => (prev === value ? null : value));
+  };
+
   return (
     <div className={styles.centerblockFilter}>
       <div className={styles.filterTitle}>Искать по:</div>
-      <div className={classNames(styles.filterButton, styles.btnText)}>
-        исполнителю
-      </div>
-      <div className={classNames(styles.filterButton, styles.btnText)}>
-        году выпуска
-      </div>
-      <div className={classNames(styles.filterButton, styles.btnText)}>
-        жанру
-      </div>
+      {filterData.map((item, index) => (
+          <TracksFilterCategory
+            key={index}
+            title={item.title}
+            list={item.list}
+            onClick={handleFilterValue}
+            value={item.value}
+            isOpen={filterValue === item.value}
+          />
+        ))}
     </div>
   );
 }
--- music/src/components/navMenu/navMenu.tsx
+++ music/src/components/navMenu/navMenu.tsx
@@ -1,8 +1,14 @@
+'use client'
+
 import Image from "next/image";
 import classNames from "classnames";
 import styles from "./navMenu.module.css";
+import { useState } from "react";
 
 export default function NawMenu() {
+  const [visible, setVisible] = useState(false);
+  const toggleVisibility = () => setVisible(!visible);
+
   return (
     <nav className={classNames(styles.mainNav, styles.nav)}>
       <div className={classNames(styles.navLogo, styles.logo)}>
@@ -14,11 +20,12 @@ export default function NawMenu() {
           height={17}
         />
       </div>
-      <div className={classNames(styles.navBurger, styles.burger)}>
+      <div className={classNames(styles.navBurger, styles.burger)} onClick={toggleVisibility}>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
         <span className={styles.burgerLine}></span>
       </div>
+      {visible &&
       <div className={styles.navMenu}>
         <ul className={styles.menuList}>
           <li className={styles.menuItem}>
@@ -38,6 +45,7 @@ export default function NawMenu() {
           </li>
         </ul>
       </div>
+      }
     </nav>
   );
 }
